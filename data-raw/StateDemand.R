559ab8d5cfa9e03a609e8842716c13384f80017c
MoLi7 <mo.vico.li@gmail.com>
2020-10-28 15:33:01 -0700

    Save State_ValueAdded_2012 as .rda

--- data-raw/StateDemand.R
+++ data-raw/StateDemand.R
@@ -1,3 +1,3 @@
 # Generate and save state value added
 State_Value_Added <- assembleStateValueAdded(2012)
-save(state_US_VA_ratio, file = "data/State_ValueAdded_2012.rda")
+save(State_Value_Added, file = "data/State_ValueAdded_2012.rda")
