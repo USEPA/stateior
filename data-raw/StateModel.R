0d517f6c14adaaacfe8c29a0cdd6e984e5747fcb
MoLi7 <mo.vico.li@gmail.com>
2020-11-23 16:05:22 -0800

    (cont.) Save two-region (50 states + DC and their corresponding RoUS) Make, Use, DomesticUse, IndustryOutput and CommodityOutput to .rda

--- data-raw/StateModel.R
+++ data-raw/StateModel.R
@@ -19,5 +19,23 @@ usethis::use_data(State_Summary_Use_2012, overwrite = TRUE)
 State_Summary_DomesticUse_2012 <- StateDemandModel_2012$DomesticUse
 usethis::use_data(State_Summary_DomesticUse_2012, overwrite = TRUE)
 
-### 2012 Two-region State Demand Model
-StateTwoRegionDemandModel <- buildTwoRegionStateDemandModel(state, year, ioschema, iolevel)
+### 2012 Two-region IO tables and industry and commodity output
+TwoRegion_Summary_IO_2012 <- assembleTwoRegionIO(iolevel = "Summary", 2012)
+# Make
+TwoRegion_Summary_Make_2012 <- TwoRegion_Summary_IO_2012$Make
+usethis::use_data(TwoRegion_Summary_Make_2012, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2012 <- TwoRegion_Summary_IO_2012$Use
+usethis::use_data(TwoRegion_Summary_Use_2012, overwrite = TRUE)
+# DomesticUse
+TwoRegion_Summary_DomesticUse_2012 <- TwoRegion_Summary_IO_2012$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2012, overwrite = TRUE)
+# Demand
+TwoRegion_Summary_Demand_2012 <- TwoRegion_Summary_IO_2012$Demand
+usethis::use_data(TwoRegion_Summary_Demand_2012, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2012 <- TwoRegion_Summary_IO_2012$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2012, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2012 <- TwoRegion_Summary_IO_2012$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2012, overwrite = TRUE)
