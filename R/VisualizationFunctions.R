49279cbaa5200a4a754e1c7708a0a57cbed40797
MoLi7 <mo.vico.li@gmail.com>
2021-05-24 18:15:24 -0700

    Add plotResidual function

--- R/VisualizationFunctions.R
+++ R/VisualizationFunctions.R
@@ -3,40 +3,84 @@
 #' @import ggplot2
 NULL
 
-#' Plot SoI2SoI ICF and state RPC
+#' Plot SoI2SoI or RoUS2RoUS ICF and state RPC
 #' @param ICF_df A data.frame contains SoI2SoI ICF ratios
 #' @param RPC_df A data.frame contains state RPC ratios
 #' @export
 plotICFandRPC <- function(ICF_df, RPC_df) {
   # ICF
-  ICF_df_long <- reshape2::melt(ICF_df[, !colnames(ICF_df)%in%c("source", "source (w/ manual adjustment)")],
+  adjust_by <- unique(ICF_df$AdjustBy)
+  icf_remove_cols <- c("source", "source (adjusted)", "AdjustBy")
+  ICF_df_long <- reshape2::melt(ICF_df[, !colnames(ICF_df)%in%icf_remove_cols],
                                 id.vars = c("Sector", "Name"))
   # RPC
   RPC_df <- merge(RPC_df, unique(ICF_df_long[, c("Sector", "Name")]), by = "Sector")
-  RPC_df_long <- reshape2::melt(RPC_df[, !colnames(RPC_df)%in%c("OverallRPC", "OverallRPC (w/ manual adjustment)")],
+  rpc_remove_cols <- c("OverallRPC", "OverallRPC (adjusted)")
+  RPC_df_long <- reshape2::melt(RPC_df[, !colnames(RPC_df)%in%rpc_remove_cols],
                                 id.vars = c("Sector", "Name"))
   # plot
   SectorName <- ICF_df[, c("Sector", "Name")]
   p <- ggplot(ICF_df_long, aes(x = value, y = factor(Name, levels = rev(SectorName$Name)))) +
     # ICF
     geom_line(aes(group = Sector)) +
-    geom_point(data = ICF_df_long[ICF_df_long$variable=="SoI2SoI_ICF (w/ manual adjustment)", ],
+    geom_point(data = ICF_df_long[ICF_df_long$variable==unique(ICF_df_long$variable)[2], ],
                aes(color = variable), size = 3) +
-    geom_point(data = ICF_df_long[ICF_df_long$variable=="SoI2SoI_ICF", ],
+    geom_point(data = ICF_df_long[ICF_df_long$variable==unique(ICF_df_long$variable)[1], ],
                aes(color = variable), size = 3) +
     # RPC
     geom_line(data = RPC_df_long[RPC_df_long$value>=0, ], aes(group = Sector)) +
     geom_point(data = RPC_df_long[RPC_df_long$value>=0, ], aes(shape = variable), size = 3) +
+    # Overall RPC
+    geom_vline(data = RPC_df, aes(xintercept = mean(RPC_df[, "OverallRPC"]),
+                                  linetype = "Overall RPC")) +
+    geom_vline(data = RPC_df, aes(xintercept = mean(RPC_df[, "OverallRPC (adjusted)"]),
+                                  linetype = "Overall RPC (adjusted)")) +
     # general aesthetics
-    labs(x = "", y = "") +
-    scale_x_continuous(breaks = scales::pretty_breaks(),
-                       sec.axis = sec_axis(~., name = "", breaks = scales::pretty_breaks())) +
-    #scale_y_discrete(labels = function(x) stringr::str_wrap(x, 50)) +
+    labs(x = "", y = "", shape = "", linetype = "",
+         col = paste("Adjusted ICF =", adjust_by, "+ ICF * (1 -", paste0(adjust_by, ")"))) +
+    scale_x_continuous(breaks = scales::breaks_pretty(n = 10),
+                       sec.axis = sec_axis(~., name = "",
+                                           breaks = scales::breaks_pretty(n = 10))) +
     theme_linedraw(base_size = 15) +
     theme(axis.text = element_text(color = "black", size = 15),
-          axis.text.y = element_text(size = 15),
-          axis.title.x = element_text(size = 10), axis.ticks = element_blank(),
-          legend.title = element_blank(), legend.position = "top",
-          legend.text = element_text(size = 15), legend.key.size = unit(1, "cm"))
+          axis.ticks = element_blank(), panel.grid.minor.x = element_blank(),
+          legend.text = element_text(size = 15), legend.key.size = unit(1, "cm")) +
+    guides(color = guide_legend(order = 1),
+           shape = guide_legend(order = 2),
+           linetype = guide_legend(order = 3))
+  return(p)
+}
+
+#' Plot SoI2SoI ICF and state RPC
+#' @param df A data.frame contains residual values
+#' @export
+plotResidual <- function(df) {
+  adjust_by <- unique(df$AdjustBy)
+  keep_cols <- c("Sector", "Name", "residual", "residual (adjusted)")
+  df_long <- reshape2::melt(df[, colnames(df)%in%keep_cols],
+                            id.vars = c("Sector", "Name"))
+  df_long$value <- df_long$value * 1E-9
+  SectorName <- df[, c("Sector", "Name")]
+  x_lab <- "Residual (SoI Imports from RoUS - RoUS Exports to SoI, in billion $)"
+  # plot
+  p <- ggplot(df_long, aes(x = value,
+                           y = factor(Name, levels = rev(SectorName$Name)))) +
+    geom_line(aes(group = Sector)) +
+    geom_point(data = df_long[df_long$variable==unique(df_long$variable)[2], ],
+               aes(color = variable), size = 3) +
+    geom_point(data = df_long[df_long$variable==unique(df_long$variable)[1], ],
+               aes(color = variable), size = 3) +
+    geom_vline(xintercept = 0) +
+    # general aesthetics
+    labs(x = x_lab, y = "", col = paste("Adjusted ICF =", adjust_by, "+ ICF * (1 -",
+                                        paste0(adjust_by, ")"))) +
+    scale_x_continuous(breaks = scales::breaks_pretty(),
+                       sec.axis = sec_axis(~., name = x_lab,
+                                           breaks = scales::breaks_pretty())) +
+    theme_linedraw(base_size = 15) +
+    theme(axis.text = element_text(color = "black", size = 15),
+          axis.ticks = element_blank(),
+          legend.text = element_text(size = 15),
+          legend.key.size = unit(1, "cm"))
   return(p)
 }
