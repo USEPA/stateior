315d5a70f285531d70ab4501de3ab600c385c117
MoLi7 <mo.vico.li@gmail.com>
2021-09-17 10:07:32 -0700

    Update StateModel.R

--- data-raw/StateModel.R
+++ data-raw/StateModel.R
@@ -1,4 +1,4 @@
-### 2012 State Supply Model
+### 2012 State Supply Model ###
 StateSupplyModel_2012 <- buildStateSupplyModel(2012)
 # Make table
 State_Summary_Make_2012 <- StateSupplyModel_2012$Make
@@ -10,7 +10,7 @@ usethis::use_data(State_Summary_IndustryOutput_2012, overwrite = TRUE)
 State_Summary_CommodityOutput_2012 <- StateSupplyModel_2012$CommodityOutput
 usethis::use_data(State_Summary_CommodityOutput_2012, overwrite = TRUE)
 
-### 2012 State Demand Model
+### 2012 State Demand Model ###
 StateDemandModel_2012 <- buildStateDemandModel(2012)
 # Use table
 State_Summary_Use_2012 <- StateDemandModel_2012$Use
@@ -19,23 +19,112 @@ usethis::use_data(State_Summary_Use_2012, overwrite = TRUE)
 State_Summary_DomesticUse_2012 <- StateDemandModel_2012$DomesticUse
 usethis::use_data(State_Summary_DomesticUse_2012, overwrite = TRUE)
 
-### 2012 Two-region IO tables and industry and commodity output
-TwoRegion_Summary_IO_2012 <- assembleTwoRegionIO(year = 2012, iolevel = "Summary")
-# Make
-TwoRegion_Summary_Make_2012 <- TwoRegion_Summary_IO_2012$Make
-usethis::use_data(TwoRegion_Summary_Make_2012, overwrite = TRUE)
-# Use
-TwoRegion_Summary_Use_2012 <- TwoRegion_Summary_IO_2012$Use
-usethis::use_data(TwoRegion_Summary_Use_2012, overwrite = TRUE)
-# DomesticUse
-TwoRegion_Summary_DomesticUse_2012 <- TwoRegion_Summary_IO_2012$DomesticUse
-usethis::use_data(TwoRegion_Summary_DomesticUse_2012, overwrite = TRUE)
-# Demand
-TwoRegion_Summary_Demand_2012 <- TwoRegion_Summary_IO_2012$Demand
-usethis::use_data(TwoRegion_Summary_Demand_2012, overwrite = TRUE)
+
+### 2013 State Supply Model ###
+StateSupplyModel_2013 <- buildStateSupplyModel(2013)
+# Make table
+State_Summary_Make_2013 <- StateSupplyModel_2013$Make
+usethis::use_data(State_Summary_Make_2013, overwrite = TRUE)
 # Industry Output
-TwoRegion_Summary_IndustryOutput_2012 <- TwoRegion_Summary_IO_2012$IndustryOutput
-usethis::use_data(TwoRegion_Summary_IndustryOutput_2012, overwrite = TRUE)
+State_Summary_IndustryOutput_2013 <- StateSupplyModel_2013$IndustryOutput
+usethis::use_data(State_Summary_IndustryOutput_2013, overwrite = TRUE)
 # Commodity Output
-TwoRegion_Summary_CommodityOutput_2012 <- TwoRegion_Summary_IO_2012$CommodityOutput
-usethis::use_data(TwoRegion_Summary_CommodityOutput_2012, overwrite = TRUE)
+State_Summary_CommodityOutput_2013 <- StateSupplyModel_2013$CommodityOutput
+usethis::use_data(State_Summary_CommodityOutput_2013, overwrite = TRUE)
+
+### 2013 State Demand Model ###
+StateDemandModel_2013 <- buildStateDemandModel(2013)
+# Use table
+State_Summary_Use_2013 <- StateDemandModel_2013$Use
+usethis::use_data(State_Summary_Use_2013, overwrite = TRUE)
+# Domestic Use table
+State_Summary_DomesticUse_2013 <- StateDemandModel_2013$DomesticUse
+usethis::use_data(State_Summary_DomesticUse_2013, overwrite = TRUE)
+
+
+### 2014 State Supply Model ###
+StateSupplyModel_2014 <- buildStateSupplyModel(2014)
+# Make table
+State_Summary_Make_2014 <- StateSupplyModel_2014$Make
+usethis::use_data(State_Summary_Make_2014, overwrite = TRUE)
+# Industry Output
+State_Summary_IndustryOutput_2014 <- StateSupplyModel_2014$IndustryOutput
+usethis::use_data(State_Summary_IndustryOutput_2014, overwrite = TRUE)
+# Commodity Output
+State_Summary_CommodityOutput_2014 <- StateSupplyModel_2014$CommodityOutput
+usethis::use_data(State_Summary_CommodityOutput_2014, overwrite = TRUE)
+
+### 2014 State Demand Model ###
+StateDemandModel_2014 <- buildStateDemandModel(2014)
+# Use table
+State_Summary_Use_2014 <- StateDemandModel_2014$Use
+usethis::use_data(State_Summary_Use_2014, overwrite = TRUE)
+# Domestic Use table
+State_Summary_DomesticUse_2014 <- StateDemandModel_2014$DomesticUse
+usethis::use_data(State_Summary_DomesticUse_2014, overwrite = TRUE)
+
+
+### 2015 State Supply Model ###
+StateSupplyModel_2015 <- buildStateSupplyModel(2015)
+# Make table
+State_Summary_Make_2015 <- StateSupplyModel_2015$Make
+usethis::use_data(State_Summary_Make_2015, overwrite = TRUE)
+# Industry Output
+State_Summary_IndustryOutput_2015 <- StateSupplyModel_2015$IndustryOutput
+usethis::use_data(State_Summary_IndustryOutput_2015, overwrite = TRUE)
+# Commodity Output
+State_Summary_CommodityOutput_2015 <- StateSupplyModel_2015$CommodityOutput
+usethis::use_data(State_Summary_CommodityOutput_2015, overwrite = TRUE)
+
+### 2015 State Demand Model ###
+StateDemandModel_2015 <- buildStateDemandModel(2015)
+# Use table
+State_Summary_Use_2015 <- StateDemandModel_2015$Use
+usethis::use_data(State_Summary_Use_2015, overwrite = TRUE)
+# Domestic Use table
+State_Summary_DomesticUse_2015 <- StateDemandModel_2015$DomesticUse
+usethis::use_data(State_Summary_DomesticUse_2015, overwrite = TRUE)
+
+
+### 2016 State Supply Model ###
+StateSupplyModel_2016 <- buildStateSupplyModel(2016)
+# Make table
+State_Summary_Make_2016 <- StateSupplyModel_2016$Make
+usethis::use_data(State_Summary_Make_2016, overwrite = TRUE)
+# Industry Output
+State_Summary_IndustryOutput_2016 <- StateSupplyModel_2016$IndustryOutput
+usethis::use_data(State_Summary_IndustryOutput_2016, overwrite = TRUE)
+# Commodity Output
+State_Summary_CommodityOutput_2016 <- StateSupplyModel_2016$CommodityOutput
+usethis::use_data(State_Summary_CommodityOutput_2016, overwrite = TRUE)
+
+### 2016 State Demand Model ###
+StateDemandModel_2016 <- buildStateDemandModel(2016)
+# Use table
+State_Summary_Use_2016 <- StateDemandModel_2016$Use
+usethis::use_data(State_Summary_Use_2016, overwrite = TRUE)
+# Domestic Use table
+State_Summary_DomesticUse_2016 <- StateDemandModel_2016$DomesticUse
+usethis::use_data(State_Summary_DomesticUse_2016, overwrite = TRUE)
+
+
+### 2017 State Supply Model ###
+StateSupplyModel_2017 <- buildStateSupplyModel(2017)
+# Make table
+State_Summary_Make_2017 <- StateSupplyModel_2017$Make
+usethis::use_data(State_Summary_Make_2017, overwrite = TRUE)
+# Industry Output
+State_Summary_IndustryOutput_2017 <- StateSupplyModel_2017$IndustryOutput
+usethis::use_data(State_Summary_IndustryOutput_2017, overwrite = TRUE)
+# Commodity Output
+State_Summary_CommodityOutput_2017 <- StateSupplyModel_2017$CommodityOutput
+usethis::use_data(State_Summary_CommodityOutput_2017, overwrite = TRUE)
+
+### 2017 State Demand Model ###
+StateDemandModel_2017 <- buildStateDemandModel(2017)
+# Use table
+State_Summary_Use_2017 <- StateDemandModel_2017$Use
+usethis::use_data(State_Summary_Use_2017, overwrite = TRUE)
+# Domestic Use table
+State_Summary_DomesticUse_2017 <- StateDemandModel_2017$DomesticUse
+usethis::use_data(State_Summary_DomesticUse_2017, overwrite = TRUE)
