095a9337d6fd776450ff92e64ecd798c09484e5f
MoLi7 <mo.vico.li@gmail.com>
2021-09-21 09:48:02 -0700

    Update StateSupply.R and StateDemand.R

--- data-raw/StateDemand.R
+++ data-raw/StateDemand.R
@@ -1,6 +1,23 @@
-# Generate and save state gross value added
+# Generate and save 2012 state gross value added
 State_Summary_GrossValueAdded_2012 <- assembleStateSummaryGrossValueAdded(2012)
 usethis::use_data(State_Summary_GrossValueAdded_2012, overwrite = TRUE)
 
-# State_Summary_GrossValueAdded_2017 <- assembleStateSummaryGrossValueAdded(2017)
-# usethis::use_data(State_Summary_GrossValueAdded_2017, overwrite = TRUE)
+# Generate and save 2013 state gross value added
+State_Summary_GrossValueAdded_2013 <- assembleStateSummaryGrossValueAdded(2013)
+usethis::use_data(State_Summary_GrossValueAdded_2013, overwrite = TRUE)
+
+# Generate and save 2014 state gross value added
+State_Summary_GrossValueAdded_2014 <- assembleStateSummaryGrossValueAdded(2014)
+usethis::use_data(State_Summary_GrossValueAdded_2014, overwrite = TRUE)
+
+# Generate and save 2015 state gross value added
+State_Summary_GrossValueAdded_2015 <- assembleStateSummaryGrossValueAdded(2015)
+usethis::use_data(State_Summary_GrossValueAdded_2015, overwrite = TRUE)
+
+# Generate and save 2016 state gross value added
+State_Summary_GrossValueAdded_2016 <- assembleStateSummaryGrossValueAdded(2016)
+usethis::use_data(State_Summary_GrossValueAdded_2016, overwrite = TRUE)
+
+# Generate and save 2017 state gross value added
+State_Summary_GrossValueAdded_2017 <- assembleStateSummaryGrossValueAdded(2017)
+usethis::use_data(State_Summary_GrossValueAdded_2017, overwrite = TRUE)
