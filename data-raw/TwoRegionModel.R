cfcaf7980524429ea2a9f0eab3dd3e0e82d221dc
MoLi7 <mo.vico.li@gmail.com>
2022-01-20 14:47:46 -0800

    Add TwoRegionModel.R for saving two-region tables as final data

---
+++ data-raw/TwoRegionModel.R
@@ -0,0 +1,143 @@
+### 2012 ###
+TwoRegion_Summary_IO_2012 <- assembleTwoRegionIO(year = 2012, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2012 <- TwoRegion_Summary_IO_2012$Make
+usethis::use_data(TwoRegion_Summary_Make_2012, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2012 <- TwoRegion_Summary_IO_2012$Use
+usethis::use_data(TwoRegion_Summary_Use_2012, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2012 <- TwoRegion_Summary_IO_2012$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2012, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2012 <- TwoRegion_Summary_IO_2012$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2012, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2012 <- TwoRegion_Summary_IO_2012$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2012, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2012 <- TwoRegion_Summary_IO_2012$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2012, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2012 <- TwoRegion_Summary_IO_2012$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2012, overwrite = TRUE)
+
+### 2013 ###
+TwoRegion_Summary_IO_2013 <- assembleTwoRegionIO(year = 2013, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2013 <- TwoRegion_Summary_IO_2013$Make
+usethis::use_data(TwoRegion_Summary_Make_2013, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2013 <- TwoRegion_Summary_IO_2013$Use
+usethis::use_data(TwoRegion_Summary_Use_2013, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2013 <- TwoRegion_Summary_IO_2013$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2013, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2013 <- TwoRegion_Summary_IO_2013$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2013, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2013 <- TwoRegion_Summary_IO_2013$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2013, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2013 <- TwoRegion_Summary_IO_2013$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2013, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2013 <- TwoRegion_Summary_IO_2013$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2013, overwrite = TRUE)
+
+### 2014 ###
+TwoRegion_Summary_IO_2014 <- assembleTwoRegionIO(year = 2014, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2014 <- TwoRegion_Summary_IO_2014$Make
+usethis::use_data(TwoRegion_Summary_Make_2014, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2014 <- TwoRegion_Summary_IO_2014$Use
+usethis::use_data(TwoRegion_Summary_Use_2014, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2014 <- TwoRegion_Summary_IO_2014$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2014, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2014 <- TwoRegion_Summary_IO_2014$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2014, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2014 <- TwoRegion_Summary_IO_2014$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2014, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2014 <- TwoRegion_Summary_IO_2014$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2014, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2014 <- TwoRegion_Summary_IO_2014$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2014, overwrite = TRUE)
+
+### 2015 ###
+TwoRegion_Summary_IO_2015 <- assembleTwoRegionIO(year = 2015, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2015 <- TwoRegion_Summary_IO_2015$Make
+usethis::use_data(TwoRegion_Summary_Make_2015, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2015 <- TwoRegion_Summary_IO_2015$Use
+usethis::use_data(TwoRegion_Summary_Use_2015, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2015 <- TwoRegion_Summary_IO_2015$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2015, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2015 <- TwoRegion_Summary_IO_2015$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2015, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2015 <- TwoRegion_Summary_IO_2015$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2015, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2015 <- TwoRegion_Summary_IO_2015$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2015, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2015 <- TwoRegion_Summary_IO_2015$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2015, overwrite = TRUE)
+
+### 2016 ###
+TwoRegion_Summary_IO_2016 <- assembleTwoRegionIO(year = 2016, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2016 <- TwoRegion_Summary_IO_2016$Make
+usethis::use_data(TwoRegion_Summary_Make_2016, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2016 <- TwoRegion_Summary_IO_2016$Use
+usethis::use_data(TwoRegion_Summary_Use_2016, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2016 <- TwoRegion_Summary_IO_2016$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2016, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2016 <- TwoRegion_Summary_IO_2016$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2016, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2016 <- TwoRegion_Summary_IO_2016$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2016, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2016 <- TwoRegion_Summary_IO_2016$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2016, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2016 <- TwoRegion_Summary_IO_2016$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2016, overwrite = TRUE)
+
+### 2017 ###
+TwoRegion_Summary_IO_2017 <- assembleTwoRegionIO(year = 2017, iolevel = "Summary")
+# Make
+TwoRegion_Summary_Make_2017 <- TwoRegion_Summary_IO_2017$Make
+usethis::use_data(TwoRegion_Summary_Make_2017, overwrite = TRUE)
+# Use
+TwoRegion_Summary_Use_2017 <- TwoRegion_Summary_IO_2017$Use
+usethis::use_data(TwoRegion_Summary_Use_2017, overwrite = TRUE)
+# Domestic Use
+TwoRegion_Summary_DomesticUse_2017 <- TwoRegion_Summary_IO_2017$DomesticUse
+usethis::use_data(TwoRegion_Summary_DomesticUse_2017, overwrite = TRUE)
+# Domestic Use (with interregional exports and imports)
+TwoRegion_Summary_DomesticUsewithTrade_2017 <- TwoRegion_Summary_IO_2017$DomesticUsewithTrade
+usethis::use_data(TwoRegion_Summary_DomesticUsewithTrade_2017, overwrite = TRUE)
+# Value Added
+TwoRegion_Summary_ValueAdded_2017 <- TwoRegion_Summary_IO_2017$ValueAdded
+usethis::use_data(TwoRegion_Summary_ValueAdded_2017, overwrite = TRUE)
+# Industry Output
+TwoRegion_Summary_IndustryOutput_2017 <- TwoRegion_Summary_IO_2017$IndustryOutput
+usethis::use_data(TwoRegion_Summary_IndustryOutput_2017, overwrite = TRUE)
+# Commodity Output
+TwoRegion_Summary_CommodityOutput_2017 <- TwoRegion_Summary_IO_2017$CommodityOutput
+usethis::use_data(TwoRegion_Summary_CommodityOutput_2017, overwrite = TRUE)
