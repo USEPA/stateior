a8e134544bd4779812f78d58176d8ea07390e1ad
MoLi7 <mo.vico.li@gmail.com>
2022-02-28 09:16:08 -0800

    Split saving intermediate data into saving supply and use intermediate data

--- data-raw/StateSupplyIntermediateData.R
+++ data-raw/StateSupplyIntermediateData.R
@@ -1,5 +1,7 @@
+# For state supply model, generate and save (1) state-US value added ratios and
+# (2) state alternative commodity output ratios
 for (year in 2012:2017) {
-  for (name in c("VAR", "COR", "GVA")) {
+  for (name in c("VAR", "COR")) {
     if (name=="VAR") {
       # Generate and save state-US value added ratios
       df <- finalizeStateUSValueAddedRatio(year)
@@ -17,13 +19,6 @@ for (year in 2012:2017) {
                       "National Oceanic and Atmospheric Administration",
                       "US Oak Ridge National Laboratory",
                       sep = ",")
-    } else if (name=="GVA") {
-      # Generate and save alternative state gross value added
-      df <- assembleStateSummaryGrossValueAdded(year)
-      data_name <- paste("State_Summary_GrossValueAdded", year,
-                         utils::packageDescription("stateior", fields = "Version"),
-                         sep = "_")
-      source <- "US Bureau of Economic Analysis"
     }
     # Write data to .rds
     saveRDS(object = df,
