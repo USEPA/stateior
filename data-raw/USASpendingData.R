b6a4556fc6b3caf9d90879ac49d3a1076532fd13
MoLi7 <mo.vico.li@gmail.com>
2022-05-06 16:49:02 -0700

    Update how USAspending data are saved; parse archived data name from website to create working url

--- data-raw/USASpendingData.R
+++ data-raw/USASpendingData.R
@@ -8,10 +8,10 @@ getFedGovSpending <- function(year) {
                                      package = "stateior"),
                          stringsAsFactors = FALSE, check.names = FALSE)
   # Prepare PSC codes by category
-  psc_ls <- list(PSC[PSC$Purchase_Type=="Intermediate", "4_Digit_PSC"],
-                 PSC[PSC$Final_Demand_Category=="Equipment", "4_Digit_PSC"],
-                 PSC[PSC$Final_Demand_Category=="Intellectual_Property", "4_Digit_PSC"],
-                 PSC[PSC$Final_Demand_Category=="Structures", "4_Digit_PSC"])
+  psc_ls <- list(PSC[PSC$Purchase_Type == "Intermediate", "4_Digit_PSC"],
+                 PSC[PSC$Final_Demand_Category == "Equipment", "4_Digit_PSC"],
+                 PSC[PSC$Final_Demand_Category == "Intellectual_Property", "4_Digit_PSC"],
+                 PSC[PSC$Final_Demand_Category == "Structures", "4_Digit_PSC"])
   names(psc_ls) <- c("Intermediate", "Equipment", "IP", "Structure")
   # Load NAICS table
   NAICStoPull <- utils::read.csv(system.file("extdata",
@@ -22,15 +22,22 @@ getFedGovSpending <- function(year) {
   # Because the original data is by fiscal year (Oct-Sept) instead of calendar year (Jan-Dec)
   # Download and load data from all years to compile complete data by calendar year
   # Need get 2012-2017 data, extend the range to 2011-2018 to retrieve complete records
-  year_range <- (year-1):(year+1)
+  year_range <- (year - 1):(year + 1)
   df_ls <- list()
   df_ls[as.character(year_range)] <- data.frame()
+  # Get data_date for archived award data
+  notes <- readLines("https://files.usaspending.gov/database_download")
+  tmp <- readLines("https://www.usaspending.gov/download_center/award_data_archive")
+  data_date <- gsub("-", "",
+                    stringr::str_match(notes[grep("USAspending Database", notes)],
+                                       "Database (.*?) zip")[2])
   for (year_N in year_range) {
     # Create the placeholder file
-    FedGovExpzip <- paste0("inst/extdata/FY", year_N, "_All_Contracts_Full_20220110.zip")
-    if(!file.exists(FedGovExpzip)) {
+    FedGovExpzip <- paste0("inst/extdata/FY", year_N, "_All_Contracts_Full_",
+                           data_date, ".zip")
+    if (!file.exists(FedGovExpzip)) {
       utils::download.file(paste0("https://files.usaspending.gov/award_data_archive/FY",
-                                  year_N, "_All_Contracts_Full_20220110.zip"),
+                                  year_N, "_All_Contracts_Full_", data_date, ".zip"),
                            FedGovExpzip, mode = "wb",
                            timeout = max(500, getOption("timeout")))
     }
@@ -51,11 +58,13 @@ getFedGovSpending <- function(year) {
       # Define filename
       filename <- paste0("inst/extdata/USAspending/", fname[i])
       # Load data
-      df_slice <- data.table::fread(filename, select = columns, stringsAsFactors = FALSE)
+      df_slice <- as.data.frame(data.table::fread(filename,
+                                                  select = columns,
+                                                  stringsAsFactors = FALSE))
       # Subset data
-      df_slice <- unique(df_slice[df_slice$primary_place_of_performance_country_code=="USA" &
-                                    df_slice$award_type_code%in%c("A", "B", "C", "D") &
-                                    df_slice$primary_place_of_performance_state_code%in%c(state.abb, "DC") &
+      df_slice <- unique(df_slice[df_slice$primary_place_of_performance_country_code == "USA" &
+                                    df_slice$award_type_code %in% c("A", "B", "C", "D") &
+                                    df_slice$primary_place_of_performance_state_code %in% c(state.abb, "DC") &
                                     !is.na(df_slice$federal_action_obligation) &
                                     !is.na(df_slice$naics_code) &
                                     df_slice$federal_action_obligation >= 0, ])
@@ -66,9 +75,9 @@ getFedGovSpending <- function(year) {
     }
     # Paste df in df_ls
     df_ls[[as.character(year_N)]] <- rbind(df_ls[[as.character(year_N)]],
-                                           df[df$Year==year_N, ])
-    df_ls[[as.character(year_N-1)]] <- rbind(df_ls[[as.character(year_N-1)]],
-                                             df[df$Year==(year_N-1), ])
+                                           df[df$Year == year_N, ])
+    df_ls[[as.character(year_N - 1)]] <- rbind(df_ls[[as.character(year_N - 1)]],
+                                               df[df$Year == (year_N - 1), ])
   }
   # Subset data
   # Load df for the year
@@ -77,13 +86,13 @@ getFedGovSpending <- function(year) {
   for (category in names(psc_ls)) {
     # Extract records for Defense and Non-Defense (based on awarding agency and PSC code)
     for (type in c("Defense", "NonDefense")) {
-      if (type=="Defense") {
-        df_type <- df_year[df_year$product_or_service_code%in%psc_ls[[category]] &
-                             df_year$awarding_agency_name=="DEPARTMENT OF DEFENSE (DOD)" |
-                             df_year$product_or_service_code%in%PSC[grep("DEFENSE", PSC$PSC_Code), "4_Digit_PSC"], ]
+      if (type == "Defense") {
+        df_type <- df_year[df_year$product_or_service_code %in% psc_ls[[category]] &
+                             df_year$awarding_agency_name == "DEPARTMENT OF DEFENSE (DOD)" |
+                             df_year$product_or_service_code %in% PSC[grep("DEFENSE", PSC$PSC_Code), "4_Digit_PSC"], ]
       } else {
-        df_type <- df_year[df_year$product_or_service_code%in%psc_ls[[category]] &
-                             df_year$awarding_agency_name!="DEPARTMENT OF DEFENSE (DOD)", ]
+        df_type <- df_year[df_year$product_or_service_code %in% psc_ls[[category]] &
+                             df_year$awarding_agency_name != "DEPARTMENT OF DEFENSE (DOD)", ]
       }
       # Aggregate
       df <- stats::aggregate(df_type$federal_action_obligation,
@@ -103,7 +112,9 @@ getFedGovSpending <- function(year) {
                                     name = data_name,
                                     year = year,
                                     source = "USA spending",
-                                    url = "https://www.usaspending.gov/download_center/award_data_archive")
+                                    url = "https://www.usaspending.gov/download_center/award_data_archive",
+                                    date_last_modified = "unknown",
+                                    date_accessed = as.character(Sys.Date()))
     }
   }
 }
