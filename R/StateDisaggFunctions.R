49a8e64e61e8bb49f44f4b3ac632b3ab2c41d87e
jvendries <jvendries@hotmail.com>
2022-06-04 19:28:35 -0400

    added functionality to start disaggregating the state make tables in the stateDisaggFunctions.R script

--- R/StateDisaggFunctions.R
+++ R/StateDisaggFunctions.R
@@ -40,11 +40,66 @@
 #' }
 
 
-#' @param model An stateior model object with model specs and IO tables loaded
-#' @param configpaths str vector, paths (including file name) of disagg configuration file(s).
-#' If NULL, built-in config files are used.
-#' @return A model with the specified aggregation and disaggregation specs.
-getStateDisaggregationSpecs <- function(model, configpaths){
-  #TODO: Need to make this work with calls to useeior::
+
+#' Gets a stored or user specified model or aggregation/disaggregation configuration file
+#' @param configname str, name of the configuration file
+#' @param configtype str, configuration type, can be "model", "disagg", or "agg"
+#' @param configpaths str vector, paths (including file name) of model configuration file
+#' and optional agg/disagg configuration file(s). If NULL, built-in config files are used.
+#' @return A list of model specifications.
+getConfigurationStateior <- function(configname, configtype, configpaths = NULL) {
+  configfile <- paste0(configname, ".yml")
+  if (is.null(configpaths)) {
+    configpath <- system.file(paste0("extdata/", configtype, "specs/"), configfile, package = "stateior")
+  } else {
+    configpath <- configpaths[endsWith(configpaths, configfile)]
+  }
+  if (!file.exists(configpath)) {
+    stop(paste(configfile, "must be available in ", dirname(configpath)),
+         call. = FALSE)
+  }
+  config <- configr::read.config(configpath)
+  return(config)
+}
+
+
+#' @param model An stateior model object with model specs and specific IO tables loaded
+#' @param state A string value that indicates the state model being disaggregated
+#' @return A stateior model with the disaggregateed objects
+disaggregateStateModel <- function(model, state){
+
+  temp <- 1
+  # Need to export all disagg functions from useeior package? 
+  disaggregateMakeTable <- utils::getFromNamespace("disaggregateMakeTable","useeior")
+  
+
+  
+  for (disagg in model$DisaggregationSpecs){
+    # TODO: add call to other disaggregation functions
+    
+    
+    # Format stateior model objects to conform to useeior disaggregation formats
+    # Format for MakeFileDF
+    oldIndIndeces <- which(!(disagg$MakeFileDF$IndustryCode %in% disagg$DisaggregatedSectorCodes)) # Find Ind indeces for MakeFileDF which do not contain new sectors 
+    disagg$MakeFileDF$IndustryCode[oldIndIndeces] <- gsub("\\/.*", "",disagg$MakeFileDF$IndustryCode[oldIndIndeces]) # remove the /US from oldIndIndces
+    disagg$MakeFileDF$IndustryCode[oldIndIndeces] <- paste0(state,".",disagg$MakeFileDF$IndustryCode[oldIndIndeces]) # add state name to oldIndIndeces sectors
+    
+    #same as above, but for commodity codes in the make table
+    oldComIndeces <- which(!(disagg$MakeFileDF$CommodityCode %in% disagg$DisaggregatedSectorCodes))
+    disagg$MakeFileDF$CommodityCode[oldComIndeces] <- gsub("\\/.*", "",disagg$MakeFileDF$CommodityCode[oldComIndeces]) # remove the /US from oldIndIndces
+    
+    # Format for UseFileDF...
+    
+    model$MakeTransactions <- disaggregateMakeTable(model, disagg)
+    
+  }
+  
+  
+
+  
+  temp <- 2
+  
+  return(model)
+  
   
 }
