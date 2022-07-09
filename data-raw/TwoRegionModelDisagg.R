8337a6e8c8b1d93a13b00406f90e08d5c64f13f8
Ben Young <Ben.Young@erg.com>
2022-07-08 21:20:22 -0400

    update actions to match changes in original two-region action

--- data-raw/TwoRegionModelDisagg.R
+++ data-raw/TwoRegionModelDisagg.R
@@ -1,24 +1,22 @@
 # Generate and save two-region IO tables
-for (year in 2017:2017) {
-  # Build model
-  TwoRegionModel <- assembleTwoRegionIO(year, iolevel = "Summary", disaggState = TRUE)
-  # Subset data set
-  for (name in names(TwoRegionModel)) {
-    df <- TwoRegionModel[[name]]
-    # Write data to .rds
-    data_name <- paste("TwoRegion_Summary", name, "UtilDisagg", year,
-                       utils::packageDescription("stateior", fields = "Version"),
-                       sep = "_")
-    saveRDS(object = df,
-            file = paste0(file.path("data", data_name), ".rds"))
-    # Write metadata to JSON
-    useeior:::writeMetadatatoJSON(package = "stateior",
-                                  name = data_name,
-                                  year = year,
-                                  source = "stateior",
-                                  url = NULL,
-                                  date_last_modified = as.character(Sys.Date()),
-                                  date_accessed = NULL)
-  }
+# Build model
+TwoRegionModel <- assembleTwoRegionIO(year, iolevel = "Summary", disaggState = TRUE)
+# Subset data set
+for (name in names(TwoRegionModel)) {
+  df <- TwoRegionModel[[name]]
+  # Write data to .rds
+  data_name <- paste("TwoRegion_Summary", name, "UtilDisagg", year,
+                     utils::packageDescription("stateior", fields = "Version"),
+                     sep = "_")
+  saveRDS(object = df,
+          file = paste0(file.path("data", data_name), ".rds"))
+  # Write metadata to JSON
+  useeior:::writeMetadatatoJSON(package = "stateior",
+                                name = data_name,
+                                year = year,
+                                source = "stateior",
+                                url = NULL,
+                                date_last_modified = as.character(Sys.Date()),
+                                date_accessed = NULL)
 }
 
