7ec6dc1dee49446b3fd245350a58ebf727ef059c
MoLi7 <mo.vico.li@gmail.com>
2022-08-02 18:09:24 -0700

    Set download timeout to 1000 second in save-raw-data.yaml

--- data-raw/USASpendingData.R
+++ data-raw/USASpendingData.R
@@ -37,8 +37,7 @@ getFedGovSpending <- function(year) {
       if (!file.exists(FedGovExpzip)) {
         utils::download.file(paste0("https://files.usaspending.gov/award_data_archive/FY",
                                     year_N, "_All_Contracts_Full_", data_date, ".zip"),
-                             FedGovExpzip, mode = "wb",
-                             timeout = max(1000, getOption("timeout")))
+                             FedGovExpzip, mode = "wb")
       }
       # Get the name of all files in the zip archive
       fname <- unzip(FedGovExpzip, list = TRUE)[unzip(FedGovExpzip, list = TRUE)$Length > 0, ]$Name
