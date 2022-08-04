c00766f2d84230d0e2b80f9beef46b8860401e54
MoLi7 <mo.vico.li@gmail.com>
2022-08-03 17:28:49 -0700

    Set up parallel runs for saving data from state supply and use models

--- data-raw/StateSupplyModel.R
+++ data-raw/StateSupplyModel.R
@@ -1,23 +1,21 @@
 # Generate and save state Make tables
-for (year in 2012:2017) {
-  # Build model
-  StateSupplyModel <- buildStateSupplyModel(year)
-  # Subset data set
-  for (name in names(StateSupplyModel)) {
-    df <- StateSupplyModel[[name]]
-    # Write data to .rds
-    data_name <- paste("State_Summary", name, year,
-                       utils::packageDescription("stateior", fields = "Version"),
-                       sep = "_")
-    saveRDS(object = df,
-            file = paste0(file.path("data", data_name), ".rds"))
-    # Write metadata to JSON
-    useeior:::writeMetadatatoJSON(package = "stateior",
-                                  name = data_name,
-                                  year = year,
-                                  source = "stateior",
-                                  url = NULL,
-                                  date_last_modified = as.character(Sys.Date()),
-                                  date_accessed = NULL)
-  }
+# Build model
+StateSupplyModel <- buildStateSupplyModel(year)
+# Subset data set
+for (name in names(StateSupplyModel)) {
+  df <- StateSupplyModel[[name]]
+  # Write data to .rds
+  data_name <- paste("State_Summary", name, year,
+                     utils::packageDescription("stateior", fields = "Version"),
+                     sep = "_")
+  saveRDS(object = df,
+          file = paste0(file.path("data", data_name), ".rds"))
+  # Write metadata to JSON
+  useeior:::writeMetadatatoJSON(package = "stateior",
+                                name = data_name,
+                                year = year,
+                                source = "stateior",
+                                url = NULL,
+                                date_last_modified = as.character(Sys.Date()),
+                                date_accessed = NULL)
 }
