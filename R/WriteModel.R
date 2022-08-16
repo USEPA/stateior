1ff1748d242c84e458f41e831815aec276ef3965
MoLi7 <mo.vico.li@gmail.com>
2022-08-16 09:59:30 -0700

    Add write data function

---
+++ R/WriteModel.R
@@ -0,0 +1,19 @@
+#' Write StateIO data to specified format, .csv or .xlsx, to output folder.
+#' The data are loaded from Data Commons or local data directory.
+#' @param filename A string specifying filename, e.g. "State_Summary_Use_2017".
+#' @param ver A string specifying version of the data, default is NULL, can be "v0.1.0".
+#' @param outputfolder A directory to write matrices out to.
+#' @description Writes StateIO data as .csv or .xlsx files to output folder.
+#' @export
+writeStateIODatatoCSV <- function(filename, ver = NULL, state, outputfolder) {
+  data <- loadStateIODataFile(filename, ver)[[state]]
+  if (!dir.exists(outputfolder)) {
+    dir.create(outputfolder, recursive = TRUE) 
+  }
+  utils::write.csv(data, file.path(outputfolder, "/", matrix, ".csv"),
+                   na = "", row.names = TRUE, fileEncoding = "UTF-8")
+  logging::loginfo(paste0(state, unlist(strsplit(filename, "_"))[4],
+                          unlist(strsplit(filename, "_"))[3],
+                          " written to ", outputfolder, "."))
+}
+
