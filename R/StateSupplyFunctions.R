183495a57c314b354b30f2e9348e4944051e4441
Brandon Istenes <bistenes@gmail.com>
2024-12-10 23:30:25 -0500

    Add get employment table function. Improve loadStateIODataFile docs

--- R/StateSupplyFunctions.R
+++ R/StateSupplyFunctions.R
@@ -295,6 +295,23 @@ estimateStateCommodityOutputRatiofromAlternativeSources <- function(year) {
   return(StateCommodityOutputRatio)
 }
 
+#' Get a table of state employment by BEA Summary sector.
+#' @param year A numeric value specifying the year of interest.
+#' @return A data frame containing employment count by state and BEA Summary sector.
+#' @export
+getStateEmploymentTable <- function(year) {
+  BEAStateEmp <- loadStateIODataFile(paste0("State_Employment_", year))
+  loadBEAStateDatatoBEASummaryMapping("Employment")
+  EmptoBEAmapping <- loadBEAStateDatatoBEASummaryMapping("Employment")
+  BEAStateEmp <- merge(BEAStateEmp[, c("GeoName", "LineCode", as.character(year))],
+                        EmptoBEAmapping, by = "LineCode")
+  BEAStateEmp <- stats::aggregate(BEAStateEmp[, as.character(year)],
+                                  by = list(BEAStateEmp$BEA_2012_Summary_Code,
+                                            BEAStateEmp$GeoName), sum)
+  colnames(BEAStateEmp) <- c("BEA_2012_Summary_Code", "State", "Emp")
+  return(BEAStateEmp)
+}
+
 #' Load BEA State Employment data from pre-saved .rds files and State Employment
 #' FlowBySector data from flowsa.
 #' Map to BEA Summary sectors.
