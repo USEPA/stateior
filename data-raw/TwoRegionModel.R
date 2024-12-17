cb97a41eea9fc97b3e7fd1e7da0daea86e1fab99
Ben Young <ben.young@erg.com>
2024-12-17 10:54:24 -0500

    use alias for state model files

--- data-raw/TwoRegionModel.R
+++ data-raw/TwoRegionModel.R
@@ -1,26 +1,24 @@
 # Generate and save two-region IO tables
-## Temp code inputs:
-# model_spec <- "StateIOv1.0.0-75"
-# model_spec <- "StateIOv1.0.0-s"
-# year <- 2017
+
+# model_spec <- "StateIOv1.2-milkbar" # Utilities disaggregation
+# model_spec <- "StateIOv1.2-shoofly" # base model
+# year <- 2019
 
 # Load model spec
 logging::loginfo(paste("Generating two region model for", model_spec))
 configpath <- system.file("extdata/modelspecs/", paste0(model_spec, ".yml"), package = "stateior")
 specs <- configr::read.config(configpath)
-# specs <- useeior:::getConfiguration(model_spec, "model", pkg="stateior")
 
 # Build model
 TwoRegionModel <- assembleTwoRegionIO(year, iolevel = specs$BaseIOLevel,
                                       disagg_specs = specs$DisaggregationSpecs)
+alias <- gsub("^.*-", "", model_spec)
 
 # Subset data set
 for (name in names(TwoRegionModel)) {
   df <- TwoRegionModel[[name]]
   # Write data to .rds
-  name <- ifelse(is.null(specs$DisaggregationSpecs), name,
-                 paste0(name, "_", specs$DisaggregationSpecs))
-  data_name <- paste("TwoRegion_Summary", name, year,
+  data_name <- paste("TwoRegion_Summary", name, alias, year,
                      utils::packageDescription("stateior", fields = "Version"),
                      sep = "_")
   saveRDS(object = df,
